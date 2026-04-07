5c301674158968e12594f425638b2849d5f11584
Giampierr <132006463+Giampierr@users.noreply.github.com>
2026-04-07 14:02:29 -0500

    Pequeño diseño para la tienda

---
+++ src/Tienda.java
@@ -0,0 +1,18 @@
+public class Tienda {
+    public String miTienda(){
+        StringBuilder sb=new StringBuilder();
+        sb.append("        _________\n");
+        sb.append("       /  OPEN   \\\n");
+        sb.append("      / TIENDAMAS \\\n");
+        sb.append("     /_____________\\\n");
+        sb.append("     |   _     _   |\n");
+        sb.append("     |  | |   | |  |\n");
+        sb.append("     |  |_|   |_|  |\n");
+        sb.append("     |             |\n");
+        sb.append("     |    ___      |\n");
+        sb.append("     |   |   |     |\n");
+        sb.append("     |   |___|     |\n");
+        sb.append("     |_____________|\n");
+        return sb.toString();
+    }
+      }
